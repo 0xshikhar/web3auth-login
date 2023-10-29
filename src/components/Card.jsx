9bda7ab30249618b92714281bed60d7a2474d48e
0xShikhar <0xshikhar@gmail.com>
2023-10-30 01:27:12 +0530

    integrated web3 auth and rainbowkit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,27 +2,289 @@ import Router, { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import { HiOutlineMail } from "react-icons/hi";
+import { Web3AuthNoModal } from "@web3auth/no-modal";
+import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
+import {
+    CHAIN_NAMESPACES,
+    IProvider,
+    WALLET_ADAPTERS,
+} from "@web3auth/base";
+import {
+    OpenloginAdapter,
+    OpenloginLoginParams,
+} from "@web3auth/openlogin-adapter";
+import CustomRainbowWallet from "./CustomRainbowWallet";
+
+
+const clientId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID;
 
 function Card() {
     const router = useRouter();
     const [email, setEmail] = useState('');
     const [emailError, setEmailError] = useState(false);
-
     const [open, setOpen] = useState("client");
+
+    const [web3auth, setWeb3auth] = useState();
+    const [provider, setProvider] = useState();
+    const [loggedIn, setLoggedIn] = useState();
+
+    useEffect(() => {
+        const init = async () => {
+            try {
+                const chainConfig = {
+                    chainNamespace: CHAIN_NAMESPACES.EIP155,
+                    chainId: "0x1",
+                    rpcTarget: "https://rpc.ankr.com/eth",
+                    displayName: "Ethereum Goerli",
+                    blockExplorer: "https://goerli.etherscan.io",
+                    ticker: "ETH",
+                    tickerName: "Ethereum",
+                };
+                const web3auth = new Web3AuthNoModal({
+                    clientId,
+                    chainConfig,
+                    web3AuthNetwork: "sapphire_devnet",
+                });
+
+                const privateKeyProvider = new EthereumPrivateKeyProvider({
+                    config: { chainConfig },
+                });
+
+                const openloginAdapter = new OpenloginAdapter({
+                    adapterSettings: {
+                        whiteLabel: {
+                            appName: "ByteReach",
+                            appUrl: "https://web3auth.io",
+                            logoLight: "https://web3auth.io/images/w3a-L-Favicon-1.svg",
+                            logoDark: "https://web3auth.io/images/w3a-D-Favicon-1.svg",
+                            defaultLanguage: "en", // en, de, ja, ko, zh, es, fr, pt, nl
+                            mode: "auto", // whether to enable dark mode. defaultValue: false
+                            theme: {
+                                primary: "#768729",
+                            },
+                            useLogoLoader: true,
+                        },
+                        mfaSettings: {
+                            deviceShareFactor: {
+                                enable: true,
+                                priority: 1,
+                                mandatory: true,
+                            },
+                            backUpShareFactor: {
+                                enable: true,
+                                priority: 2,
+                                mandatory: false,
+                            },
+                            socialBackupFactor: {
+                                enable: true,
+                                priority: 3,
+                                mandatory: false,
+                            },
+                            passwordFactor: {
+                                enable: true,
+                                priority: 4,
+                                mandatory: false,
+                            },
+                        },
+                    },
+                    loginSettings: {
+                        mfaLevel: "mandatory",
+                    },
+                    privateKeyProvider,
+                });
+                web3auth.configureAdapter(openloginAdapter);
+                setWeb3auth(web3auth);
+
+                // adding wallet connect v2 adapter
+                // const defaultWcSettings = await getWalletConnectV2Settings(
+                //   "eip155",
+                //   [1],
+                //   "04309ed1007e77d1f119b85205bb779d"
+                // );
+                // const walletConnectModal = new WalletConnectModal( {projectId: "04309ed1007e77d1f119b85205bb779d"});
+                // const walletConnectV2Adapter = new WalletConnectV2Adapter({
+                //   adapterSettings: {
+                //     qrcodeModal: walletConnectModal,
+                //     ...defaultWcSettings.adapterSettings,
+                //   },
+                //   loginSettings: { ...defaultWcSettings.loginSettings },
+                // });
+
+                // web3auth.configureAdapter(walletConnectV2Adapter);
+
+                await web3auth.init();
+                setProvider(web3auth.provider);
+                if (web3auth.connected) {
+                    setLoggedIn(true);
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        init();
+    }, []);
+
+    const login = async () => {
+        if (!web3auth) {
+            uiConsole("web3auth not initialized yet");
+            return;
+        }
+        const web3authProvider = await web3auth.connectTo(
+            WALLET_ADAPTERS.OPENLOGIN,
+            {
+                loginProvider: "google",
+            }
+        );
+        setProvider(web3authProvider);
+    };
+
+    const loginWithEmail = async (email) => {
+        if (!web3auth) {
+            uiConsole("web3auth not initialized yet");
+            return;
+        }
+        const web3authProvider = await web3auth.connectTo(
+            WALLET_ADAPTERS.OPENLOGIN,
+            {
+                loginProvider: "email_passwordless",
+                extraLoginOptions: {
+                    login_hint: email,
+                },
+            }
+        );
+        setProvider(web3authProvider);
+    };
+
+    const authenticateUser = async () => {
+        if (!web3auth) {
+            uiConsole("web3auth not initialized yet");
+            return;
+        }
+        const idToken = await web3auth.authenticateUser();
+        uiConsole(idToken);
+    };
+
+    const getUserInfo = async () => {
+        if (!web3auth) {
+            uiConsole("web3auth not initialized yet");
+            return;
+        }
+        const user = await web3auth.getUserInfo();
+        uiConsole(user);
+    };
+
+    const logout = async () => {
+        if (!web3auth) {
+            uiConsole("web3auth not initialized yet");
+            return;
+        }
+        await web3auth.logout();
+        setProvider(null);
+        setLoggedIn(false);
+    };
+
+    const getChainId = async () => {
+        if (!provider) {
+            uiConsole("provider not initialized yet");
+            return;
+        }
+        const rpc = new RPC(provider);
+        const chainId = await rpc.getChainId();
+        uiConsole(chainId);
+    };
+
+    const addChain = async () => {
+        if (!provider) {
+            uiConsole("provider not initialized yet");
+            return;
+        }
+        const newChain = {
+            chainId: "0x5",
+            displayName: "Goerli",
+            chainNamespace: CHAIN_NAMESPACES.EIP155,
+            tickerName: "Goerli",
+            ticker: "ETH",
+            decimals: 18,
+            rpcTarget: "https://rpc.ankr.com/eth_goerli",
+            blockExplorer: "https://goerli.etherscan.io",
+        };
+        await web3auth?.addChain(newChain);
+        uiConsole("New Chain Added");
+    };
+
+    const switchChain = async () => {
+        if (!provider) {
+            uiConsole("provider not initialized yet");
+            return;
+        }
+        await web3auth?.switchChain({ chainId: "0x5" });
+        uiConsole("Chain Switched");
+    };
+
+    const getAccounts = async () => {
+        if (!provider) {
+            uiConsole("provider not initialized yet");
+            return;
+        }
+        const rpc = new RPC(provider);
+        const address = await rpc.getAccounts();
+        uiConsole(address);
+    };
+
+    const getBalance = async () => {
+        if (!provider) {
+            uiConsole("provider not initialized yet");
+            return;
+        }
+        const rpc = new RPC(provider);
+        const balance = await rpc.getBalance();
+        uiConsole(balance);
+    };
+
+    const sendTransaction = async () => {
+        if (!provider) {
+            uiConsole("provider not initialized yet");
+            return;
+        }
+        const rpc = new RPC(provider);
+        const receipt = await rpc.sendTransaction();
+        uiConsole(receipt);
+    };
+
+    const signMessage = async () => {
+        if (!provider) {
+            uiConsole("provider not initialized yet");
+            return;
+        }
+        const rpc = new RPC(provider);
+        const signedMessage = await rpc.signMessage();
+        uiConsole(signedMessage);
+    };
+
+    const getPrivateKey = async () => {
+        if (!provider) {
+            uiConsole("provider not initialized yet");
+            return;
+        }
+        const rpc = new RPC(provider);
+        const privateKey = await rpc.getPrivateKey();
+        uiConsole(privateKey);
+    };
+
 
     const handleTabOpen = (tabCategory) => {
         setOpen(tabCategory);
     };
 
-
     const handleSubmit = (e) => {
         e.preventDefault();
         // You can add your email submission logic here, like making an API request to send the email.
         console.log('Email submitted:', email);
+        loginWithEmail(email);
     };
 
     const handleOnChange = (e) => {
-
         setEmail(e.target.value);
         let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         if (re.test(email)) {
@@ -34,8 +296,6 @@ function Card() {
             setEmailError(true);
         }
     }
-
-
 
     return (
         <>
@@ -66,21 +326,19 @@ function Card() {
                                             </button>
                                         </div>
                                     </div>
-
                                 </div>
-
                             </div>
                         </div>
                         {
                             open === "client" ? (
-                                <div className="px-2 md:px-10 pt-3 w-full md:h-[300px]">
+                                <div className="px-2 md:px-10 pt-3 w-full md:h-[310px]">
                                     <div className="flex text-center justify-center items-center align-middle mt-3 ">
-                                        <button className="px-10 items-center justify-center py-3 border flex gap-6 border-slate-200 text-center dark:border-slate-700 bg-white w-full rounded-[20px] text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
+                                        <button onClick={login} className="px-10 items-center justify-center py-3 border flex gap-6 border-slate-200 text-center dark:border-slate-700 bg-white w-full rounded-[20px] text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
                                             <Image src="https://www.svgrepo.com/show/475656/google-color.svg" alt="google logo" width={25} height={25} />
                                             <div className="text-black">Login using your Google Account</div>
                                         </button>
                                     </div>
-                                    <div class=" h-[0px] border my-6 border-neutral-300"></div>
+                                    <div className=" h-[0px] border my-6 border-neutral-300"></div>
                                     <div className="text-black">
                                         <div className="text-left px-2 md:px-0"> Email
                                         </div>
@@ -93,9 +351,9 @@ function Card() {
                                                     value={email}
                                                     onChange={(e) => handleOnChange(e)}
                                                     required
-                                                    className="peer px-10 py-3 border flex gap-6 border-slate-200  bg-white w-full rounded-[20px] text-slate-700 dark:text-slate-600 hover:border-slate-200 hover:shadow transition duration-150"
+                                                    className="peer px-10 py-3 border flex gap-6 border-slate-200  bg-white w-full rounded-[20px] text-slate-700 dark:text-slate-600 hover:border-slate-200 "
                                                 />
-                                                {emailError ? (<p class="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
+                                                {emailError ? (<p className=" invisible peer-invalid:visible text-red-800 text-sm">
                                                     Please provide a valid email address.
                                                 </p>)
                                                     :
@@ -117,7 +375,7 @@ function Card() {
                                 (
                                     <div className="px-2 md:px-10 pt-3 w-full md:h-[450px]">
                                         <div className="flex text-center justify-center items-center align-middle mt-3 ">
-                                            <button className="px-10 items-center justify-center py-3 border flex gap-6 border-slate-200 text-center dark:border-slate-700 bg-white w-full rounded-[20px] text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
+                                            <button onClick={login} className="px-10 items-center justify-center py-3 border flex gap-6 border-slate-200 text-center dark:border-slate-700 bg-white w-full rounded-[20px] text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
                                                 <Image src="https://www.svgrepo.com/show/475656/google-color.svg" alt="google logo" width={25} height={25} />
                                                 <div className="text-black">Login using your Google Account</div>
                                             </button>
@@ -129,12 +387,9 @@ function Card() {
                                             </button>
                                         </div>
                                         <div className="flex text-center justify-center items-center align-middle mt-3 ">
-                                            <button className="px-10 items-center justify-center py-3 border flex gap-6 border-slate-200 text-center dark:border-slate-700 bg-white w-full rounded-[20px] text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
-                                                <Image src="/wallet.png" alt="google logo" width={25} height={25} />
-                                                <div className="text-black">Login using your Wallet</div>
-                                            </button>
+                                            <CustomRainbowWallet />
                                         </div>
-                                        <div class=" h-[0px] border my-6 border-neutral-300"></div>
+                                        <div className=" h-[0px] border my-6 border-neutral-300"></div>
                                         <div className="text-black">
                                             <div className="text-left px-2 md:px-0"> Email
                                             </div>
@@ -149,7 +404,7 @@ function Card() {
                                                         required
                                                         className="peer px-10 py-3 border flex gap-6 border-slate-200  bg-white w-full rounded-[20px] text-slate-700 dark:text-slate-600 hover:border-slate-200 hover:shadow transition duration-150"
                                                     />
-                                                    {emailError ? (<p class="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
+                                                    {emailError ? (<p className="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
                                                         Please provide a valid email address.
                                                     </p>)
                                                         :
@@ -177,4 +432,4 @@ function Card() {
 
 export default Card;
 
-{/* <div class="w-[656px] h-[427px] bg-zinc-100 rounded-[34px] shadow border border-zinc-100"></div> */ }
+{/* <div className="w-[656px] h-[427px] bg-zinc-100 rounded-[34px] shadow border border-zinc-100"></div> */ }
---
+++ src/components/CustomRainbowWallet.jsx
@@ -0,0 +1,101 @@
+import Image from 'next/image';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
+const CustomRainbowWallet = () => {
+    return (
+        <ConnectButton.Custom>
+            {({
+                account,
+                chain,
+                openAccountModal,
+                openChainModal,
+                openConnectModal,
+                authenticationStatus,
+                mounted,
+            }) => {
+                // Note: If your app doesn't use authentication, you
+                // can remove all 'authenticationStatus' checks
+                const ready = mounted && authenticationStatus !== 'loading';
+                const connected =
+                    ready &&
+                    account &&
+                    chain &&
+                    (!authenticationStatus ||
+                        authenticationStatus === 'authenticated');
+                return (
+                    <div
+                        {...(!ready && {
+                            'aria-hidden': true,
+                            'style': {
+                                opacity: 0,
+                                pointerEvents: 'none',
+                                userSelect: 'none',
+                            },
+                        })}
+                    >
+                        {(() => {
+                            if (!connected) {
+                                return (
+                                    <button onClick={openConnectModal} type="button" className="w-full px-10 md:px-[174px] items-center justify-center py-3 border flex gap-6 border-slate-200 text-center dark:border-slate-700 bg-white rounded-[20px] text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
+
+                                        <Image src="/wallet.png" alt="google logo" width={25} height={25} />
+
+                                        <div className="text-black">Login using your Wallet</div>
+
+                                    </button>
+                                );
+                            }
+                            if (chain.unsupported) {
+                                return (
+                                    <button onClick={openChainModal} type="button">
+                                        Wrong network
+                                    </button>
+                                );
+                            }
+                            return (
+                                <div className='text-black' style={{ display: 'flex', gap: 12 }}>
+                                    <button
+                                        onClick={openChainModal}
+                                        style={{ display: 'flex', alignItems: 'center' }}
+                                        type="button"
+                                        
+                                    >
+                                        {chain.hasIcon && (
+                                            <div
+                                                style={{
+                                                    background: chain.iconBackground,
+                                                    width: 12,
+                                                    height: 12,
+                                                    borderRadius: 999,
+                                                    overflow: 'hidden',
+                                                    marginRight: 4,
+                                                }}
+                                            >
+                                                {chain.iconUrl && (
+                                                    <img
+                                                        alt={chain.name ?? 'Chain icon'}
+                                                        src={chain.iconUrl}
+                                                        style={{ width: 12, height: 12 }}
+                                                    />
+                                                )}
+                                            </div>
+                                        )}
+                                        {chain.name}
+                                    </button>
+                                    <button onClick={openAccountModal} type="button" >
+                                        {account.displayName}
+                                        {account.displayBalance
+                                            ? ` (${account.displayBalance})`
+                                            : ''}
+                                    </button>
+                                </div>
+                            );
+                        })()}
+                    </div>
+                );
+            }}
+        </ConnectButton.Custom >
+    );
+};
+
+export default CustomRainbowWallet;
